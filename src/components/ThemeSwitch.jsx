b9e9092901aa93b1805305e4d2e47ae8c32a9d54
m-brandao <miguel.brandao@feedzai.com>
2023-09-03 13:38:46 +0100

    theme switcher with localstorage save

--- src/components/ThemeSwitch.jsx
+++ src/components/ThemeSwitch.jsx
@@ -2,21 +2,37 @@ import React, { useState, useEffect } from 'react';
 import './ThemeSwitch.css';  // Assuming you saved the above styles in ThemeSwitch.css
 
 export default function ThemeSwitch() {
-    const [darkMode, setDarkMode] = useState(false);
+    // Check if a theme preference is already saved in local storage
+    const savedTheme = localStorage.getItem('theme');
+    const initialDarkMode = savedTheme === 'dark';
+
+    // Initialize state with the saved theme preference or default to light mode
+    const [darkMode, setDarkMode] = useState(initialDarkMode);
+
+    // Function to toggle and save the theme preference
+    const toggleDarkMode = () => {
+        const newDarkMode = !darkMode;
+        setDarkMode(newDarkMode);
+
+        // Save the theme preference in local storage
+        localStorage.setItem('theme', newDarkMode ? 'dark' : 'light');
+
+        // Add or remove the 'dark' class to the document element
+        if (newDarkMode) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    };
 
     useEffect(() => {
-        const isDark = document.documentElement.classList.contains('dark');
-        setDarkMode(isDark);
-    }, []);
-
-    const toggleDarkMode = () => {
+        // Set the 'dark' class on the document element when the component mounts
         if (darkMode) {
-            document.documentElement.classList.remove('dark');
-        } else {
             document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
         }
-        setDarkMode(!darkMode);
-    };
+    }, [darkMode]);
 
     return (
         <label className="switch">
