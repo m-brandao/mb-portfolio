40e51a08d9341f81cf487efb9de2a28ebf5d5391
m-brandao <miguel.brandao@feedzai.com>
2023-12-19 18:54:38 +0000

    updating vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mb-portfolio/', // Set the base path to match your GitHub Pages repository name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Output directory for build
+    assetsDir: '.', // Relative path to assets (make sure it's correct for deployment)
+    sourcemap: false, // You can enable sourcemaps if needed
+  },
+});
