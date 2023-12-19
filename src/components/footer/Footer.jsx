61e7b5d8858b4803895421c5f550ffe113a0e0cd
m-brandao <miguel.brandao@feedzai.com>
2023-12-19 18:36:15 +0000

    adding footer to the project

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='bg-slate-100 shadow-lg dark:bg-slate-950 dark:dark-mode-shadow m-auto p-5 z-50'>
+        <h3 className='text-white text-center'>@2023 - Miguel Brandão</h3>
+      <p className='text-white text-center text-xs'>1.0-Beta</p>
+    </footer>
+  )
+}
