ba1016f8a98b9ae27546cc42e9966b951a659ca7
miguel.brandao <miguelbrandao@cofina.pt>
2024-02-21 21:53:04 +0000

    trying to fix tools icons

--- src/components/toolsListing/ToolsListing.jsx
+++ src/components/toolsListing/ToolsListing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import toolsData from '../../assets/data/tools.json';
 
+
 export default function ToolsListing({ id }) {
 
     const tool = toolsData.tools.find(tool => tool.id === id);
@@ -16,6 +17,8 @@ export default function ToolsListing({ id }) {
                     <li className='mr-1.5 mt-2' key={item}>
 
                         <div className="group flex relative">
+                            
+                            {/* <span className={item[0] !== 'PHP' ? 'w-5 h-5 flex dark:hidden' : 'w-7 h-7 flex dark:hidden'}>{item[1]}</span> */}
                             <img className={item[0] !== 'PHP' ? 'w-5 h-5 flex dark:hidden' : 'w-7 h-7 flex dark:hidden'} src={item[1]} alt="" />
                             <img className={item[0] !== 'PHP' ? 'w-5 h-5 hidden dark:flex' : 'w-7 h-7 hidden dark:flex'} src={item[2]} alt="" />
                             <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">{item[0]}</span>
