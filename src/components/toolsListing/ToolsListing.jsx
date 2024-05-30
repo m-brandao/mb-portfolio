28438f3b4042ac9727f56528aff1eba846028a1f
m-brandao <miguel.brandao@feedzai.com>
2024-05-30 12:42:34 +0100

    add mjml to the tools

--- src/components/toolsListing/ToolsListing.jsx
+++ src/components/toolsListing/ToolsListing.jsx
@@ -19,6 +19,8 @@ import SASS from "../../assets/images/sass.svg";
 import SASSWhite from "../../assets/images/sass-white.svg";
 import FIREBASE from "../../assets/images/firebase.svg";
 import FIREBASEWhite from "../../assets/images/firebase-white.svg";
+import MJML from "../../assets/images/mjml.svg";
+import MJMLWhite from "../../assets/images/mjml-white.svg";
 
 
 
@@ -37,44 +39,44 @@ export default function ToolsListing({ id }) {
             <div className='mt-2 flex items-center flex-wrap'>
                 {/* HTML5 */}
                 <div className='group flex relative mx-1'>
-                    <img className='w-5 h-5 flex dark:hidden' src={HTML5} alt="" />
-                    <img className='w-5 h-5 hidden dark:flex' src={HTML5White} alt="" />
+                    <img className='w-5 h-5 flex dark:hidden' src={HTML5} alt="html5 icon" />
+                    <img className='w-5 h-5 hidden dark:flex' src={HTML5White} alt="html5 icon" />
                     <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">HTML5</span>
                 </div>
                 {/* CSS */}
                 <div className='group flex relative mx-1'>
-                    <img className='w-5 h-5 flex dark:hidden' src={CSS} alt="" />
-                    <img className='w-5 h-5 hidden dark:flex' src={CSSWhite} alt="" />
+                    <img className='w-5 h-5 flex dark:hidden' src={CSS} alt="css3 icon" />
+                    <img className='w-5 h-5 hidden dark:flex' src={CSSWhite} alt="css3 icon" />
                     <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">CSS</span>
                 </div>
                 {/* TAILWIND CSS */}
                 <div className='group flex relative mx-1'>
-                    <img className='w-5 h-5 flex dark:hidden' src={TAILWIND} alt="" />
-                    <img className='w-5 h-5 hidden dark:flex' src={TAILWINDWhite} alt="" />
+                    <img className='w-5 h-5 flex dark:hidden' src={TAILWIND} alt="Tailwind Css icon" />
+                    <img className='w-5 h-5 hidden dark:flex' src={TAILWINDWhite} alt="Tailwind Css icon" />
                     <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">Tailwind</span>
                 </div>
                 {/* SASS */}
                 <div className='group flex relative mx-1'>
-                    <img className='w-5 h-5 flex dark:hidden' src={SASS} alt="" />
-                    <img className='w-5 h-5 hidden dark:flex' src={SASSWhite} alt="" />
+                    <img className='w-5 h-5 flex dark:hidden' src={SASS} alt="SASS icon" />
+                    <img className='w-5 h-5 hidden dark:flex' src={SASSWhite} alt="SASS icon" />
                     <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">SASS</span>
                 </div>
                 {/* PHP */}
                 <div className='group flex relative mx-1'>
-                    <img className='w-7 h-7 flex dark:hidden' src={PHP} alt="" />
-                    <img className='w-7 h-7 hidden dark:flex' src={PHPWhite} alt="" />
+                    <img className='w-7 h-7 flex dark:hidden' src={PHP} alt="PHP icon" />
+                    <img className='w-7 h-7 hidden dark:flex' src={PHPWhite} alt="PHP icon" />
                     <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">PHP</span>
                 </div>
                 {/* JS */}
                 <div className='group flex relative mx-1'>
-                    <img className='w-5 h-5 flex dark:hidden' src={JS} alt="" />
-                    <img className='w-5 h-5 hidden dark:flex' src={JSWhite} alt="" />
+                    <img className='w-5 h-5 flex dark:hidden' src={JS} alt="JS icon" />
+                    <img className='w-5 h-5 hidden dark:flex' src={JSWhite} alt="JS icon" />
                     <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">JS</span>
                 </div>
                 {/* REACT */}
                 <div className='group flex relative mx-1'>
-                    <img className='w-5 h-5 flex dark:hidden' src={REACT} alt="" />
-                    <img className='w-5 h-5 hidden dark:flex' src={REACTWhite} alt="" />
+                    <img className='w-5 h-5 flex dark:hidden' src={REACT} alt="React.js icon" />
+                    <img className='w-5 h-5 hidden dark:flex' src={REACTWhite} alt="React.js icon" />
                     <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">REACT</span>
                 </div>
                 {/* WORDPRESS */}
@@ -88,6 +90,12 @@ export default function ToolsListing({ id }) {
                     <img className='w-5 h-5 flex dark:hidden' src={FIREBASE} alt="" />
                     <img className='w-5 h-5 hidden dark:flex' src={FIREBASEWhite} alt="" />
                     <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">FIREBASE</span>
+                </div>
+                {/* MJML */}
+                <div className='group flex relative mx-1'>
+                    <img className='w-5 h-5 flex dark:hidden' src={MJML} alt="" />
+                    <img className='w-5 h-5 hidden dark:flex' src={MJMLWhite} alt="" />
+                    <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">MJML</span>
                 </div>
             </div>
         </div>
